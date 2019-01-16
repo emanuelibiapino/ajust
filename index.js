6939dfb9702fb42fa99faee966eee4c0fadd53a3
Emanuel Ibiapino <31495741+emanuelibiapino@users.noreply.github.com>
2019-01-16 10:45:53 -0300

    parte da paginação(layout)
    Regivan

--- index.js
+++ index.js
@@ -11,14 +11,38 @@
 
 var messageRef = firebase.database().ref('problemas');
   
-var probList = document.getElementById('probList');
+var probList = document.getElementById('cai');
 
 firebase.database().ref('problemas').on('value', function (snapshot) {
     probList.innerHTML = '';
     snapshot.forEach(function (item) {
-        var li = document.createElement('li');
-        li.appendChild(document.createTextNode(item.val().cep + ': ' + item.val().nomeRua + ': ' + item.val().tipo + ': ' + item.val().descricao));
-        probList.appendChild(li);
+        var p = document.createElement('p');
+		var q = document.createElement('p');
+		var r = document.createElement('p');
+		var dat = document.createElement('p');
+		var d = document.createElement('div');
+		var but1 = document.createElement('button');
+		var but2 = document.createElement('button');
+		var but3 = document.createElement('button');
+		d.classList.add("caixa");
+		but1.id = "tambem";
+		but2.id = "alterar";
+		but3.id = "excluir";
+		but1.appendChild(document.createTextNode("também tenho este problema"));
+		but2.appendChild(document.createTextNode("alterar"));
+		but3.appendChild(document.createTextNode("Excluir"));
+		p.appendChild(document.createTextNode('Localizado na:'));
+		q.appendChild(document.createTextNode('Rua: '+item.val().nomeRua+'| CEP: '+item.val().cep ));
+		r.appendChild(document.createTextNode('Tipo: '+item.val().tipo+'| Descrição: '+item.val().descricao ));
+		dat.appendChild(document.createTextNode('Data: '+item.val().data));
+		d.appendChild(p);
+		d.appendChild(q);
+		d.appendChild(r);
+		d.appendChild(dat);
+		d.appendChild(but1);
+		d.appendChild(but2);
+		d.appendChild(but3);
+		probList.appendChild(d);
     });
 });
 
@@ -34,9 +58,13 @@ function submitForm(e){
 	var nomeRua = getInputVal('nomeRua');
 	var tipo = getInputVal('tipo');
 	var descricao = getInputVal('descricao');
-	
+	var ema = firebase.auth().currentUser;
+	var email = ema.email;
+	var stat = 0;
+	var data = new Date();
+	window.alert(data);
 	//Salvar no firebase
-	saveProblema(cep, nomeRua, tipo, descricao);
+	saveProblema(cep, nomeRua, tipo, descricao,email,data,stat);
 }
 
 //função para receber valores do formulario
@@ -45,15 +73,21 @@ function getInputVal(id){
 }
 
 // salvar no firebase
-function saveProblema(cep, nomeRua, tipo, descricao){
+function saveProblema(cep, nomeRua, tipo, descricao,email,data,stat){
 	var newMessageRef = messageRef.push();
 	newMessageRef.set({
 		cep: cep,
 		nomeRua: nomeRua,
 		tipo: tipo,
-		descricao: descricao
+		descricao: descricao,
+		usuario: email,
+		data: data,
+		stat: stat
+		
 	});
 }
+
+
 
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
@@ -62,15 +96,16 @@ firebase.auth().onAuthStateChanged(function(user) {
 	document.getElementById("user_div").style.display = "block";
 	document.getElementById("login_div").style.display = "none";
 	
-	//document.getElementById("pass1").style.display = 'none';
-	//document.getElementById('problemas-t').style.display = 'block';
-	//document.getElementById("titulo_cont").innerHTML = "<h1>Problemas Relatados</h1>";
 	
 	var user = firebase.auth().currentUser;
 	
 	if(user != null){
 		var email_id = user.email;
 		document.getElementById("usuario_p").innerHTML = "Bem vindo: " + email_id;
+		document.getElementById("formulario").style.display = "none";
+		document.getElementById("titulo_cont").style.display = "none";
+		document.getElementById("relatados").style.display = "block";
+		
 	}
 	
   } else {
@@ -78,6 +113,9 @@ firebase.auth().onAuthStateChanged(function(user) {
 	
 	document.getElementById("user_div").style.display = "none";
 	document.getElementById("login_div").style.display = "block";
+	document.getElementById("relatados").style.display = "none";
+	document.getElementById("formulario").style.display = "none";
+	
   }
 });
 
@@ -93,6 +131,7 @@ function login(){
 	  
 	  window.alert("Error : " + errorMessage);
 	  // ...
+	 
 	});
 	
 	
